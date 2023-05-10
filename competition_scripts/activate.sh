59652513866c044f29a198d5c6a14c016f5fce6f
anandsubramanian123 <132885483+anandsubramanian123@users.noreply.github.com>
2023-05-11 01:45:55 +0800

    Create activate.sh

---
+++ competition_scripts/activate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Starting installation..."
+
+# activate the virtual environment
+echo "Activating the virtual environment..."
+source teamname_taskB_venv/bin/activate
+echo "Virtual environment activated!"
