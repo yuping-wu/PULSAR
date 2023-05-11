2a4478bb9ab7756a8ccfbd09515ae62f28e0d312
anandsubramanian123 <132885483+anandsubramanian123@users.noreply.github.com>
2023-05-11 11:33:00 +0800

    Update decode_taskB_run1.sh

--- competition_scripts/decode_taskB_run1.sh
+++ competition_scripts/decode_taskB_run1.sh
@@ -1,13 +1,13 @@
 echo "Downloading the model..."
-wget https://kant.cs.man.ac.uk/data/public/clef-flan-t5-large-bs4-i476-o214-e3-lr3e5-noh.tar.gz
+wget https://kant.cs.man.ac.uk/data/public/pulsar-11b.tar.gz
 echo "Model Downloaded!"
 
 echo "Extracting the model..."
-tar -xvf clef-flan-t5-large-bs4-i476-o214-e3-lr3e5-noh.tar.gz
+tar -xvf pulsar-11b.tar.gz
 echo "Extracted the model!"
 
 
 echo "Running inference"
-$MODEL_PATH="clef-flan-t5-large-bs4-i476-o214-e3-lr3e5-noh/2023/epoch2"
-python inference_t5.py output/epoch_2_saved $1 taskB_PULSAR_run1.csv
+$MODEL_PATH="pulsar-11b/"
+python inference_t5.py $MODEL_PATH $1 taskB_PULSAR_run1.csv
 echo "Inference completed!"
