2807feae368d063be643b3a478b9aa27a1ea7764
Yuping Wu <40853890+yuping-wu@users.noreply.github.com>
2023-05-11 02:24:11 +0100

    Create convert_statedict.py

---
+++ fine-tune/convert_statedict.py
@@ -0,0 +1,18 @@
+# re-save model
+
+import torch
+from transformers import AutoConfig, AutoTokenizer, AutoModelForSeq2SeqLM
+
+checkpoint_path = 'output/epoch_2/pytorch_model.bin'
+model_name = 'google/flan-t5-xxl'
+
+config = AutoConfig.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+model = AutoModelForSeq2SeqLM.from_config(config)
+print('Done with loading model from config')
+model.load_state_dict(torch.load(checkpoint_path, map_location='cpu'))
+print('Done with loading model state dict')
+
+# save model
+model.save_pretrained('output/epoch_2_saved/')
