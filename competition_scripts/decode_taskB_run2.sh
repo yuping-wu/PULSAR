2b2b80437ebdb0c84d06d513bd5f7b8e326050e3
anandsubramanian123 <132885483+anandsubramanian123@users.noreply.github.com>
2023-05-11 13:13:23 +0800

    Update decode_taskB_run2.sh

--- competition_scripts/decode_taskB_run2.sh
+++ competition_scripts/decode_taskB_run2.sh
@@ -3,11 +3,11 @@ wget https://kant.cs.man.ac.uk/data/public/flan-t5-11b.tar.gz
 echo "Model Downloaded!"
 
 echo "Extracting the model..."
-tar -xvf flan-t5-11b.tar.gz
+mkdir flan-t5-11b
+tar -xvf flan-t5-11b.tar.gz -C flan-t5-11b
 echo "Extracted the model!"
 
-
 echo "Running inference"
-$MODEL_PATH="flan-t5-11b"
-python inference_t5.py $MODEL_PATH $1 taskB_PULSAR_run2.csv
+python inference_t5.py flan-t5-11b $1 taskB_PULSAR_run2.csv
 echo "Inference completed!"
+
