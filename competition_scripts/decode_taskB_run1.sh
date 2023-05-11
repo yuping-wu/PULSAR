24ff7825fdd5f9f799955af7b126e7f64767f18a
anandsubramanian123 <132885483+anandsubramanian123@users.noreply.github.com>
2023-05-11 13:10:55 +0800

    Fix tar extraction logic for downloaded model

--- competition_scripts/decode_taskB_run1.sh
+++ competition_scripts/decode_taskB_run1.sh
@@ -3,11 +3,10 @@ wget https://kant.cs.man.ac.uk/data/public/pulsar-11b.tar.gz
 echo "Model Downloaded!"
 
 echo "Extracting the model..."
-tar -xvf pulsar-11b.tar.gz
+mkdir pulsar11b
+tar -xvf pulsar-11b.tar.gz -C pulsar-11b
 echo "Extracted the model!"
 
-
 echo "Running inference"
-$MODEL_PATH="pulsar-11b/"
-python inference_t5.py $MODEL_PATH $1 taskB_PULSAR_run1.csv
+python inference_t5.py pulsar-11b $1 taskB_PULSAR_run1.csv
 echo "Inference completed!"
